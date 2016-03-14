002cd7324f27ac5118f7555203e5bcc6ece39cbe
maidm004 <maidm004@umn.edu>
2016-03-14 15:28:40 -0500

    QICD.nonpen
    QICD for model with some penalized some nonpenalized coefficients

--- R/Adamsfunc.R
+++ R/Adamsfunc.R
@@ -111,7 +111,7 @@ QICD <- function(y, x, tau=.5,lambda=NULL, weights=NULL, beta_initial=NULL, inte
     if(intercept){ beta_initial <- c(beta_initial,0) }
     if( length(beta_initial) != nxzcol ){
       stop("beta_initial does not have length ncol(x)")
-	}
+  }
   }
 
 
@@ -188,6 +188,146 @@ QICD <- function(y, x, tau=.5,lambda=NULL, weights=NULL, beta_initial=NULL, inte
 }
 
 
+
+QICD.nonpen <- function(x, z, y, tau=.5,lambda=NULL, weights=NULL, beta_initial=NULL, intercept=TRUE,  penalty="SCAD",a=3.7, 
+                converge_criteria=1e-06, maxin=100,maxout=20, method="br",...)
+#x: input nxp matrix, of dimension nobs x nvars; each row is an observation vector. 
+#y: response variable, length n vector
+#z: spline basis matrix
+#beta: initial value for x-covariates, the default value is NULL
+#tau is the quantile value
+#lambda is the tuning parameter sequence
+#a is scale parameter, the default value is 3.7 for SCAD
+#penalty is the name of nonconvex penalty function, could be scad and mcp, the default
+#value is scad
+#intercept is a logical value,should intercept(s) be fitted (default=TRUE) or set to zero(FALSE)
+#thresh is the convergence threshold for coordinate descent and majorization minimization step.
+#Default value is 1E-6
+#maxin: maximum number of iterations for inside coordinate descent,default value is 100
+#maxout: maximum number of iterations for outside MM step,default value is 20
+{
+#######################################################
+   if(is.null(dim(x))){                                                                                    
+      stop('x needs to be a matrix with more than 1 column')
+   }
+   if(is.null(dim(z))){                                                                                    
+      stop('z needs to be a matrix with 1 or more columns')
+   }
+
+  # Make sure all lambdas are positive
+  if( lambda <= 0){
+    stop("lambda must be positive.")
+  }
+
+  # Make sure we use br or fn method ("?rq.fit.br" or "?rq.fit.fnc")
+  if( method != "br" & method != "fn"){
+    stop("Incorrect method.  Choose br or fn")
+  }
+
+  # Get some basic info
+  nyrow  <- as.integer( length(y) )
+  nzcol  <- as.integer( ncol(z) )
+  nxcol  <- as.integer( ncol(x) )
+  nxzcol <- as.integer( nxcol + nzcol + intercept ) # Total number of betas to estimate
+  p      <- as.integer( nxcol + nzcol ) # Number of covariates (not intercept)
+  nonpen <- as.integer( nzcol + intercept ) # Number of nonpenalized betas
+
+   if(dim(x)[1] != nyrow){
+      stop('length of y and rows of x do not match')
+    }
+   if(dim(z)[1] != nyrow){
+      stop('length of y and rows of z do not match')
+    }
+
+  if( is.null(beta_initial) ){
+    # Put intercept as last value
+    beta_initial <- rq.lasso.fit(x=x,y=y,tau=tau,lambda=lambda,weights=weights,intercept=intercept,...)$coefficients[c((1:p)+intercept, nonpen)]
+    beta_initial <- c( beta_initial, rep(0,nxcol) )
+  } else {
+    if(intercept){ beta_initial <- c(beta_initial[1:nxcol], 0, beta_initial[-(1:nxcol)]) }
+    if( length(beta_initial) != nxzcol ){
+      stop("beta_initial does not have length ncol(x)+ncol(z)")
+    }
+  }
+
+
+  # Check penalty functions
+  if(penalty == "SCAD"){
+      pentype <- as.integer(0)  
+  } else if (penalty == "MCP"){
+      pentype <- as.integer(1)
+  } else{
+    stop("wrong penalty function")
+  }
+
+
+  # Name the coefficients
+  rowname <- c( paste("x", 1:nxcol, sep=""), paste("z", 1:nzcol, sep="") )
+  if(intercept){
+    rowname <- c("(Intercept)", rowname )
+  }
+
+
+ # Create design matrix (Penalized, Intercept, Nonpenalized)
+  xdes <- x
+  zdes <- z
+  if( intercept ){
+    zdes <- cbind(1,zdes)
+  }
+  if( !is.null(weights) ){
+      xdes <- xdes * weights
+      zdes <- zdes * weights
+      y <- y * weights
+  }  
+
+
+  lambda.new <- lambda/nyrow
+  beta_final <- matrix( NA, ncol=1, nrow=nxzcol )
+  df <- rep( NA, length(lambda) ) # Number of nonzero coefficients
+
+  # Choose function for finding new beta and do some housekeeping
+  y         <- as.double(y)
+  xdescol   <- as.integer( ncol(xdes) )
+  zdescol   <- as.integer( ncol(zdes) )
+  tau       <- as.double(tau)
+  int       <- as.integer(intercept)
+  a         <- as.double(a)
+  thresh    <- as.double(converge_criteria)
+  maxin     <- as.integer(maxin)
+
+    beta1 <- QICDz( y=y, nyrow=nyrow, x=xdes, nxcol=xdescol, z=zdes, nzcol=zdescol, beta=beta_initial,
+              tau=tau, pentype=pentype, lambda=as.double(lambda.new), a=a,
+              thresh=thresh, maxin=maxin, maxout=maxout, method=method )
+    #final beta coefficients, Order of coefficients is linear,intercept, nonlinear
+
+  residuals <-  y - cbind(xdes,zdes) %*% beta1
+  rho <- sum(check(residuals, tau=tau ))
+
+  if(intercept){ 
+    coefficients <- c(beta1[ c( nxcol+1, 1:nxcol, (nxcol+2):nxzcol )])
+    PenRho <- rho + sum(scad( abs(coefficients[ 1+(1:nxcol) ]), lambda=lambda, a=a ))
+  } else {
+    coefficients <- c(beta1)
+    PenRho <- rho + sum(scad( abs(coefficients[ 1:nxcol ]), lambda=lambda, a=a ))
+  }
+  names(coefficients) <- rowname
+
+  sub_fit <- list(coefficients = coefficients,
+                 PenRho = PenRho,
+                 residuals = residuals,
+                 rho = rho,
+                 tau = tau,
+                 n = nyrow,
+                 penalty = penalty)
+
+  class(sub_fit) <-  c("rq.pen", "rqNC")
+
+  return( sub_fit )
+}
+
+
+
+
 QICDx <- function( y, nyrow, x, nxcol, beta, tau, intercept, pentype, lambda, a,
                     thresh, maxin, maxout, ... ){
 
@@ -212,4 +352,60 @@ QICDx <- function( y, nyrow, x, nxcol, beta, tau, intercept, pentype, lambda, a,
     }
 
   return( beta1 )
+}
+
+
+QICDz <- function( y, nyrow, x, nxcol, z, nzcol, beta, tau, intercept, pentype, lambda, a,
+                    thresh, maxin, maxout, method="br", ... ){
+
+  if( method=="br" ) zcoef.func <- shortrq.fit.br
+  if( method=="fn" ) zcoef.func <- shortrq.fit.fnb
+
+  beta1 <- beta[1:nxcol]
+  betaz <- beta[-(1:nxcol)]
+  i=0
+  distance <- thresh+1
+  xbeta1 <- x%*%beta1
+
+  while( (i < maxout) & (distance >= thresh) ){
+
+    iter <- 0
+    distance.inner <- thresh + 1
+    beta0 <- beta1
+    betaz0 <- betaz
+
+    while( (iter < maxin) & (distance.inner >= thresh) ){
+
+      beta0in <- beta1
+      betazin <- betaz
+      y.aug <- y - z%*%betazin
+      out <- .C("QCD", as.double(y.aug), as.double(x), as.double(beta0), as.double(beta1), as.double(xbeta1),
+                     nyrow, nxcol, as.integer(0), as.double(tau),
+                     as.double(lambda), as.double(a), as.integer(pentype),
+                     as.double(thresh), as.integer(1))
+      beta1 <- out[[4]]
+      xbeta1 <- x%*%beta1
+      y.aug <- y - xbeta1
+
+      betaz <- zcoef.func(x=z, y=y.aug, tau=tau)
+      # coef(rq(y.aug ~ z+0, tau=tau, method=method))
+
+      distance.inner <- sqrt(sum( (beta1 - beta0in)^2 )) +
+                        mean( ( z%*%(betaz-betazin) )^2 )
+
+      iter <- iter+1
+
+    }
+
+    distance <- sqrt(sum( (beta1 - beta0)^2 )) +
+                mean( ( z%*%(betaz-betaz0) )^2 )
+
+    i <- i+1
+  }
+
+  if(i == maxout & distance > thresh){
+      warning(paste("did not converge after ", maxout, " iterations", sep=""))
+    }
+
+  return( c(beta1, betaz) )
 }
