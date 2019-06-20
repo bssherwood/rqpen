5e04b4e23fad4f33308240483321db9e87f241a4
bssherwood <bsherwood@gmail.com>
2019-06-20 16:28:28 -0500

    Fixed bug in rq.lasso.fit so it works with lambda sequence not specified

--- testing.r
+++ testing.r
@@ -12,4 +12,44 @@ y <- 1 + x[,1] - x[,8] + rnorm(100)
 groups <- rep(c(1,2),each=4)
 
 a <- groupMultLambda(x,y,groups,lambda=c(.001,.01),tau=.5)
+coefficients(a)
 b <- groupMultLambda(x_diff,y,groups,lambda=c(.001,.01),tau=.5)
+coefficients(b)
+
+a <- rq.group.fit(x,y,groups,lambda=.001,tau=.25,penalty="SCAD")
+coefficients(a)
+b <- rq.group.fit(x_diff,y,groups,lambda=.001,tau=.25,penalty="SCAD")
+coefficients(b)
+
+a <- rq.nc.fit(x,y,lambda=.001,tau=.25,penalty="SCAD")
+coefficients(a)
+b <- rq.nc.fit(x_diff,y,lambda=.001,tau=.25,penalty="SCAD")
+coefficients(b)
+
+folds <- randomly_assign(100,10)
+
+a <- cv.rq.pen(x,y,foldid=folds)
+coefficients(a)
+b <- cv.rq.pen(x_diff,y,foldid=folds)
+coefficients(b)
+
+a <- cv.rq.pen(x,y,foldid=folds, penalty="SCAD")
+coefficients(a)
+b <- cv.rq.pen(x_diff,y,foldid=folds, penalty="SCAD")
+coefficients(b)
+
+a <- cv.rq.pen(x,y,foldid=folds, penalty="SCAD", alg="LP")
+coefficients(a)
+b <- cv.rq.pen(x_diff,y,foldid=folds, penalty="SCAD", alg="LP")
+coefficients(b)
+
+a <- cv.rq.group.pen(x,y,groups,foldid=folds)
+coefficients(a)
+b <- cv.rq.group.pen(x_diff,y,groups,foldid=folds)
+coefficients(b)
+
+a <- cv.rq.group.pen(x,y,groups,alg="LP",foldid=folds)
+coefficients(a)
+b <- cv.rq.group.pen(x_diff,y,groups,alg="LP",foldid=folds)
+coefficients(b)
+
