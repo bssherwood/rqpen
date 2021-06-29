2098899e30795679f3acd0e3001186b867e49974
bssherwood <bssherwood@users.noreply.github.com>
2021-06-29 16:52:48 -0500

    Update testing2021.r

--- testing2021.r
+++ testing2021.r
@@ -17,6 +17,9 @@ x <- matrix(rnorm(n*p,sd=10),ncol=p)
 y <- 1 + x[,1] + 3*x[,3] - x[,8] + rt(n,3)
 g <- c(1,1,1,1,2,2,3,3)
 
+obj   <- rq.nc(x,y,tau=.25, penalty="aLasso",scalex=TRUE)
+
+
 obj9 <- rq.group.pen(x,y,groups=c(1,1,1,1,2,2,3,3),tau=.25,penalty="gAdLasso")
 
 
