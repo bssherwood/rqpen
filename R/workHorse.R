ab011810f3262aa9068f9f46fbcf0e88dc67c6f6
bssherwood <bssherwood@users.noreply.github.com>
2021-07-19 16:08:34 -0500

    Update workHorse.R

--- R/workHorse.R
+++ R/workHorse.R
@@ -1048,14 +1048,13 @@ print.rq.pen.seq <- function(x,...){
 	na <- length(x$a)
     if(nt==1 & na==1){
 		print(data.frame(df=x$models$df,lambda=x$models$lambda))
+	} else if(nt > 1 & na > 1){
+		print(paste(c(paste(c("Quantile regression with ", x$penalty, " penalty for quantiles:",x$tau), collapse=" ")," and `a' tuning parameter:", x$a),collapse=" "))
+	} else if( na > 1){
+		print(paste(c(paste(c("Quantile regression with ", x$penalty, " penalty for quantile:",x$tau), collapse=" ")," and `a' tuning parameter:", x$a),collapse=" "))
 	} else{
-		if(nt > 1 & na > 1){
-			print(paste(c(paste(c("Quantile regression with ", x$penalty, " penalty for quantiles:",x$tau), collapse=" ")," and `a' tuning parameter:", x$a),collapse=" "))
-		} else if( na > 1){
-			print(paste(c(paste(c("Quantile regression with ", x$penalty, " penalty for quantile:",x$tau), collapse=" ")," and `a' tuning parameter:", x$a),collapse=" "))
-		} else{
 			print(paste(c("Quantile regression with ", x$penalty, " penalty for quantiles:",x$tau), collapse=" "))
-		}
+	}	
 }
 
 getModelCoefs <- function(x,index){
