cae68be1d834b228d10708ee1f7f0b5aa414d3fe
bssherwood <bssherwood@users.noreply.github.com>
2021-08-05 14:33:31 -0500

    Update mainFunctions.R

--- R/mainFunctions.R
+++ R/mainFunctions.R
@@ -137,7 +137,6 @@ coef.cv.rq.pen <- function(object, lambda='min',...){
 #' @param max.iter Maximum number of iterations of non-linear programming algorithms.
 #' @param converge.eps Convergence threshold for non-linear programming algorithms. 
 #' @param gamma tuning parameter for Huber loss, not applicable for non-huber algorithms. 
-#' @param ...
 #'
 #' @return
 #' @export
@@ -145,7 +144,7 @@ coef.cv.rq.pen <- function(object, lambda='min',...){
 #' @examples
 rq.pen <- function(x,y,tau=.5,lambda=NULL,penalty=c("LASSO","Ridge","ENet","aLASSO","SCAD","MCP"),a=NULL,nlambda=100,eps=ifelse(nrow(x)<ncol(x),.01,.0001), 
 	penalty.factor = rep(1, ncol(x)),alg=ifelse(sum(dim(x))<200,"huber","br"),scalex=TRUE,tau.penalty.factor=rep(1,length(tau)),
-	coef.cutoff=1e-8,max.iter=10000,converge.eps=1e-7,gamma=IQR(y)/10, ...){
+	coef.cutoff=1e-8,max.iter=10000,converge.eps=1e-7,gamma=IQR(y)/10){
 	penalty <- match.arg(penalty)
 	if(penalty=="LASSO"){
 		fit <- rq.lasso(x,y,tau,lambda,nlambda,eps,penalty.factor,alg,scalex,tau.penalty.factor,coef.cutoff,max.iter,converge.eps,gamma,...)
@@ -153,7 +152,7 @@ rq.pen <- function(x,y,tau=.5,lambda=NULL,penalty=c("LASSO","Ridge","ENet","aLAS
 		if(alg != "huber"){
 			stop("huber alg is only option for Ridge penalty")
 		}
-		fit <- rq.enet(x,y,tau,lambda,nlambda=nlambda,eps=eps,penalty.factor=penalty.factor,alg=alg,scalex=scalex,tau.penalty.factor=tau.penalty.factor,coef.cuttoff=coef.cutoff,max.iter=max.iter,converge.eps=converge.eps,gamma,...)
+		fit <-  rq.enet(x,y,tau,lambda,nlambda,eps, penalty.factor,scalex,tau.penalty.factor,a=0,max.iter,converge.eps,gamma,...)
 	} else if(penalty == "ENet"){
 		if(alg != "huber"){
 			stop("huber alg is only option for ENet penalty")
@@ -161,7 +160,7 @@ rq.pen <- function(x,y,tau=.5,lambda=NULL,penalty=c("LASSO","Ridge","ENet","aLAS
 		if(is.null(a)){
 			stop("Specify a value for a for Enet penalty")
 		}
-		fit <- rq.enet(x,y,tau,lambda,a=a,...)
+		fit <- rq.enet(x,y,tau,lambda,nlambda,eps, penalty.factor,scalex,tau.penalty.factor,a,max.iter,converge.eps,gamma,...)
 	} else if(penalty == "aLASSO" | penalty=="SCAD" | penalty == "MCP"){
 		fit <- rq.nc(x,y,tau,penalty,a,lambda,nlambda=nlambda,eps=eps,penalty.factor=penalty.factor,alg=alg,scalex=scalex,tau.penalty.factor=tau.penalty.factor,coef.cuttoff=coef.cutoff,max.iter=max.iter,converge.eps=converge.eps,gamma,...)
 	}
