9f584dd260815212afb023bc6688333059d3a7cd
Sherwood <bsherwood@gmail.com>
2023-02-17 16:56:02 -0600

    Update mainFunctions.R

--- R/mainFunctions.R
+++ R/mainFunctions.R
@@ -942,11 +942,11 @@ rq.group.pen.cv <- function(x,y,tau=.5,groups=1:ncol(x),lambda=NULL,a=NULL,cvFun
 	avals <- sapply(fit$models,modelA)
 	if(groupError){
 		btr <- byTauResults(foldErrors,tauvals,avals,fit$models,stdErr,fit$lambda)
-		gtr <- groupTauResults(foldErrors, tauvals,fit$a,avals,fit$models,tauWeights,fit$lambda)
+		gtr <- groupTauResults(foldErrors, tauvals,fit$a,avals,fit$models,tauWeights,fit$lambda,stdErr)
 	} else{
 		indErrors <- t(indErrors)/n
 		btr <- byTauResults(indErrors,tauvals,avals,fit$models,stdErr,fit$lambda)
-		gtr <- groupTauResults(indErrors, tauvals,fit$a,avals,fit$models,tauWeights,fit$lambda)
+		gtr <- groupTauResults(indErrors, tauvals,fit$a,avals,fit$models,tauWeights,fit$lambda,stdErr)
 	}
 
 	returnVal <- list(cverr = foldErrors, cvse = stdErr, fit = fit, btr=btr, gtr=gtr$returnTable, gcve=gtr$gcve,call=match.call())
