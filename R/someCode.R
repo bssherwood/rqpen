bf1127bf67e52d867e53723b03e24e03d42582ff
Sherwood <bsherwood@gmail.com>
2024-03-11 12:39:57 -0400

    Create someCode.R

---
+++ R/someCode.R
@@ -0,0 +1,16 @@
+#' Weighted Norm example
+#'
+#' This function multiplies two numbers.
+#'
+#' @param x A numeric value.
+#' @param y A numeric value.
+#'
+#' @return The product of x and y.
+#'
+#' @examples
+#' wNorm(2, 3)
+#'
+#' @export
+wNorm <- function(x, y) {
+  weightedNorm(x,y)
+}
