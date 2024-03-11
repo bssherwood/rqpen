9ff9873f23943573e7542e54bb2601f814c832fc
Sherwood <bsherwood@gmail.com>
2024-03-11 16:24:36 -0400

    update done

--- src/rqPen_init.c
+++ src/rqPen_init.c
@@ -26,6 +26,18 @@ static const R_FortranMethodDef FortranEntries[] = {
     {NULL, NULL, 0}
 };
 
+static const R_CallMethodDef CallEntries[] = {
+    {"_rqPen_stlSort", (DL_FUNC) &_rqPen_stlSort, 1},
+    {"_rqPen_findIndices", (DL_FUNC) &_rqPen_findIndices, 2},
+    {"_rqPen_rqLossAug", (DL_FUNC) &_rqPen_rqLossAug, 2},
+    {"_rqPen_rqHuberDerivAug", (DL_FUNC) &_rqPen_rqHuberDerivAug, 3},
+    {"_rqPen_negGradientAug", (DL_FUNC) &_rqPen_negGradientAug, 6},
+    {"_rqPen_weightedNorm", (DL_FUNC) &_rqPen_weightedNorm, 2},
+    {"_rqPen_solvebetaCpp", (DL_FUNC) &_rqPen_solvebetaCpp, 15},
+    {NULL, NULL, 0}
+};
+
+
 void R_init_rqPen(DllInfo *dll)
 {
     R_registerRoutines(dll, CEntries, NULL, FortranEntries, NULL);
