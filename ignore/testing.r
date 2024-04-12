c823f3070db02636fa3316870618d632166fd046
Sherwood <bsherwood@gmail.com>
2024-04-12 16:13:24 -0400

    test

--- ignore/testing.r
+++ ignore/testing.r
@@ -13,8 +13,8 @@ folds <- rqPen:::randomly_assign(n,10)
 bfit <- rq.gq.pen(X,y,taus,nlambda=5)
 bfit2 <- rq.gq.pen(X,y,taus,lambda=bfit$lambda)
 
-cvfit <- rq.gq.pen.cv(x=X, y=y, tau=taus, foldid=folds, lambda=bfit$lambda)
-cvfit2 <- cv.hrq_tau_glasso(x=X, y=y, tau=taus, folds=folds, lambda=bfit$lambda)
+cvfit <- rq.gq.pen.cv(x=X, y=y, tau=taus)
+cvfit2 <- cv.hrq_tau_glasso(x=X, y=y, tau=taus)
 4
 
 library(rqPen)
