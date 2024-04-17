20d082d7079759c4d66596ba580f1228e114d56e
Sherwood <bsherwood@gmail.com>
2024-04-17 15:29:59 -0400

    update

--- R/gqCvPen.R
+++ R/gqCvPen.R
@@ -50,7 +50,7 @@
 #' \insertRef{huberGroup}{rqPen}
 #' 
 #' @author Shaobo Li \email{shaobo.li@ku.edu} and Ben Sherwood, \email{ben.sherwood@ku.edu} 
-rq.gq.pen.cv <- function(x=NULL, y=NULL, tau=NULL, lambda=NULL, nfolds=10, cvFunc=c("rq","se"), tauWeights=NULL,  foldid=NULL, ...){
+rq.gq.pen.cv <- function(x=NULL, y=NULL, tau=NULL, lambda=NULL, nfolds=10, cvFunc=c("rq","se"), tauWeights=NULL,  foldid=NULL, printProgress=FALSE, ...){
   cvFunc <- match.arg(cvFunc)
   ## two ways to call this function
   #if(!is.null(model_obj)){
