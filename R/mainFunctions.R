9c0c534c4f72ded85f66f4e31d75f5438810121b
Sherwood <bsherwood@gmail.com>
2024-05-07 10:10:23 -0400

    updates

--- R/mainFunctions.R
+++ R/mainFunctions.R
@@ -2083,7 +2083,7 @@ coef.cv.rq.group.pen <- function(object, lambda='min',...){
 #' \insertRef{qr_cd}{rqPen}
 rq.group.pen <- function(x,y, tau=.5,groups=1:ncol(x), penalty=c("gLASSO","gAdLASSO","gSCAD","gMCP"),
 						lambda=NULL,nlambda=100,eps=ifelse(nrow(x)<ncol(x),.05,.01),alg=c("huber","br"), 
-						a=NULL, norm=2, group.pen.factor=NULL,tau.penalty.factor=rep(1,length(tau)),
+						a=NULL, norm=2, group.pen.factor=rep(1,length(groups)),tau.penalty.factor=rep(1,length(tau)),
 						scalex=TRUE,coef.cutoff=1e-8,max.iter=500,converge.eps=1e-4,gamma=IQR(y)/10, lambda.discard=TRUE,weights=NULL, ...){
 	penalty <- match.arg(penalty)
 	alg <- match.arg(alg)
